bead93f5979b838add2152c72c636a59e0f57a3b
Carlos Fins <clockwerkz@gmail.com>
2018-08-07 19:45:14 -0700

    initial set up of files

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/*
+ * Udacity Front End Developer NanoDegree Program
+ * 
+ * Premium Cat Clicker Starter Files for Video Walk-Through
+ * 
+ * By Carlos Fins
+ * 
+ * https://github.com/clockwerkz
+ * 
+ * 
+*/
+
+
+document.addEventListener('DOMContentLoaded', octopus.init);
+
+
+
+
+
